55a3e006b01f070bbf75d311330a5faa1147adc1
lilsakabighurt <khalilhall1221@outlook.com>
2020-01-07 15:43:33 -0500

    fixed timer to start on button click

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,39 @@
-var questions =[]
-var score = 
+var timeEl = document.getElementById("time");
+var timeLeft = 100;
+var start = document.getElementById("startbutton");
+
+
+//start.onclick = setTime;
+document.getElementById("startbutton").addEventListener("click", setTime());
+
+function setTime(){
+    var timerInterval = setInterval(function(){
+    timeLeft--;
+    timeEl.textContent = "time:" + timeLeft;
+    if (timeLeft === 0){
+        clearInterval(timerInterval);
+    } 
+    }, 1000)
+}
+
+
+function Quiz(questions){
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+//function that will get each question
+Quiz.getQuestionIndex = function(){
+    return this.questions[this.questionIndex];
+}
+//function that will either add score or subtract time
+Quiz.guess = function(answer){
+    if(this.getQuestionIndex().isCorrectAnswer(answer)){
+        this.score++;
+    }
+    
+
+ this.questionIndex++;  
+}
+
+var questions = [];
